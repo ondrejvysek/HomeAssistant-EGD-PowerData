8c3a0b2021b194a36560f754d46611ae1ddfccdf
ondrejvysek <ondrejv@optimalizovane-it.cz>
2025-01-19 21:50:07 +0100

    Update egd.py

--- egd.py
+++ egd.py
@@ -1,7 +1,7 @@
 import json
 from egd_api import EGDAPI
 
-api = EGDAPI("0287659a99ed667d4c37cc7f8011a239", "c773b8c1299972a1c11a6e9d6e1b2597", "859182400104958738")
+api = EGDAPI("client_id", "client_secret", "ean")
 token = api.get_token()
 if token:
     data = api.get_data() # see examples in egd_api.py
